3295a7fae5a2c2c01445c4cd1977dfbaf0583afa
Zeynat <zeynat.haytham.ghallab@gmail.com>
2025-08-12 16:01:39 +0300

    updateorder

--- src/main/java/com/evision/orderapplication/controller/Controller.java
+++ src/main/java/com/evision/orderapplication/controller/Controller.java
@@ -4,10 +4,24 @@
  */
 package com.evision.orderapplication.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import com.evision.orderapplication.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 /**
  *
  * @author Hamada Salim G Trd
  */
+@RestController
+@RequestMapping("/orders")
 public class Controller {
-    
+
+    @Autowired
+    private OrderService orderService;
+    @PutMapping("/{id}")
+    public String updateOrder(@PathVariable("id") int orderId) {
+        orderService.UpdateOrderService(); 
+        return "Order with ID " + orderId + " updated successfully";
+    }
 }
